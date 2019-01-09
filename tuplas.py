2894ebee5922d97f495fb31b43505db3684c3817
RafaelAraujoCv <45515295+RafaelAraujoCv@users.noreply.github.com>
2019-01-09 21:32:14 -0200

    dicas tuplas - lista
    dicas tuplas - lista

---
+++ tuplas.py
@@ -0,0 +1,28 @@
+lista = [2,5,2,8,5]
+palavra = "banana"
+
+print(len(lista)) #len mostra quantidade de caracteres/numero da lista
+print(len(palavra)) #len mostra quantidade de caracteres/numero da lista
+print(min(lista)) #mostra o numero minimo dalista, menor numero
+print(min(palavra)) #mostra a letra minima, mais baixa
+print(lista[3]) #printa o valor na posição terceiro(3) elemento da lista
+serie = range(0,10) #lista de 0 ate 9, inicio e fim
+print(serie)
+print(serie[0]) #mostra o valor na posição zero(0)
+dias = ["S","T","Q","Q","S","S","D"] #lista comdiasda semana, string
+print(type(dias))
+print(dias)
+dias.append("D2") #add elemento novo na lista
+print(dias)
+dias.pop() #deleta o ultimo valor da lista
+print(dias)
+dias = ("S","T","Q","Q","S","S","D")
+print(type(dias)) #mostra o tipo da variavel
+ponto = (3,5)
+
+
+
+
+
+# utilizar [] significa lista - sequencia dinamica, pode add ou remover ( .append e .pop )
+# utilizar () significa tuple - sequencia fixa, nao pode mudar
